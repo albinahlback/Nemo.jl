e81e9576eae79b8e328cbc1f9be679f80b978089
Albin Ahlbäck <albin.ahlback@gmail.com>
2021-04-30 19:14:49 +0200

    [//, divexact]-fix acb-test.jl

--- test/arb/acb-test.jl
+++ test/arb/acb-test.jl
@@ -135,18 +135,18 @@ end
    @test x + y == 6
    @test x - y == -2
    @test x * y == 8
-   @test x // y == 0.5
+   @test x / y == 0.5
 
    for T in [fmpz, fmpq, Int, BigInt, Rational{Int}, Rational{BigInt}]
 
       @test x + T(4) == 6
       @test x - T(4) == -2
       @test x * T(4) == 8
-      @test x // T(4) == 0.5
+      @test x / T(4) == 0.5
       @test T(2) + y == 6
       @test T(2) - y == -2
       @test T(2) * y == 8
-      @test T(2) // y == 0.5
+      @test T(2) / y == 0.5
       @test x ^ T(4) == 16
    end
 
@@ -154,11 +154,11 @@ end
       @test contains(x + T(4), 6)
       @test contains(x - T(4), -2)
       @test contains(x * T(4), 8)
-      @test contains(x // T(4), fmpq(1, 2))
+      @test contains(x / T(4), fmpq(1, 2))
       @test contains(T(2) + y, 6)
       @test contains(T(2) - y, -2)
       @test contains(T(2) * y, 8)
-      @test contains(T(2) // y, fmpq(1, 2))
+      @test contains(T(2) / y, fmpq(1, 2))
       @test contains(x ^ T(4), 16)
    end
 end
