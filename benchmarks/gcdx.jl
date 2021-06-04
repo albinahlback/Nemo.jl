1cdc3a71c17c754deb7216e0833478cf9f5c7d6d
Albin Ahlbäck <albin.ahlback@gmail.com>
2021-06-04 16:09:56 +0200

    Add benchmark for gcdx(::fmpz, ::fmpz)

---
+++ benchmarks/gcdx.jl
@@ -0,0 +1,62 @@
+function gcdx_bigint(a::fmpz, b::fmpz)
+  g, s, t = gcdx(BigInt(a), BigInt(b))
+  return fmpz(g), fmpz(s), fmpz(t)
+end
+
+function gcdx_fmpz(a::fmpz, b::fmpz)
+  d = ZZ()
+  x = ZZ()
+  y = ZZ()
+  ccall((:fmpz_xgcd_canonical_bezout, :libflint), Nothing,
+        (Ref{fmpz}, Ref{fmpz}, Ref{fmpz}, Ref{fmpz}, Ref{fmpz}), d, x, y, a, b)
+  return d, x, y
+end
+
+function run_gcdx_bigint(x::Vector{fmpz}, y::Vector{fmpz})
+  for ix in x, iy in y
+    gcdx_bigint(ix, iy)
+  end
+end
+    
+function run_gcdx_fmpz(x::Vector{fmpz}, y::Vector{fmpz})
+  for ix in x, iy in y
+    gcdx_fmpz(ix, iy)
+  end
+end
+
+function benchmark_gcdx()
+  print("benchmark_gcdx ...\n")
+
+  # small size
+  sp = Random.Sampler(Random.MersenneTwister(),
+                      ZZ(0):ZZ(2)^(Sys.WORD_SIZE - 2) - 1)
+  x = [rand(sp) for _ in 1:100]
+  y = [rand(sp) for _ in 1:100]
+
+  tt = @elapsed run_gcdx_bigint(x, y)
+  println("Small sized integers for BigInt-solution: $tt")
+  tt = @elapsed run_gcdx_fmpz(x, y)
+  println("Small sized integers for fmpz-solution:   $tt")
+
+  # mixed integers
+  sp = Random.Sampler(Random.MersenneTwister(),
+                      ZZ(0):ZZ(2)^Sys.WORD_SIZE)
+  x = [rand(sp) for _ in 1:100]
+  y = [rand(sp) for _ in 1:100]
+
+  tt = @elapsed run_gcdx_bigint(x, y)
+  println("Mixed sized integers for BigInt-solution: $tt")
+  tt = @elapsed run_gcdx_fmpz(x, y)
+  println("Mixed sized integers for fmpz-solution:   $tt")
+
+  # big integers
+  sp = Random.Sampler(Random.MersenneTwister(),
+                      ZZ(0):ZZ(2)^512)
+  x = [rand(sp) for _ in 1:100]
+  y = [rand(sp) for _ in 1:100]
+
+  tt = @elapsed run_gcdx_bigint(x, y)
+  println("Large sized integers for BigInt-solution: $tt")
+  tt = @elapsed run_gcdx_fmpz(x, y)
+  println("Large sized integers for fmpz-solution:   $tt")
+end
