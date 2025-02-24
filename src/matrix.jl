f23f1f30d82e4c0ae154700b22268919970ef125
Johannes Schmitt <jschmitt@posteo.eu>
2025-02-24 17:45:17 +0100

    Fix documentation: `eigenspace` does not return a `Vector` (#2034)

--- src/matrix.jl
+++ src/matrix.jl
@@ -255,11 +255,12 @@ end
 
 @doc raw"""
     eigenspace(M::MatElem{T}, lambda::T; side::Symbol = :left)
-      where T <: FieldElem -> Vector{MatElem{T}}
+      where T <: FieldElem -> MatElem{T}
 
-Return a basis of the eigenspace of $M$ with respect to the eigenvalue $\lambda$.
-If side is `:right`, the right eigenspace is computed, i.e. vectors $v$ such that
-$Mv = \lambda v$. If side is `:left`, the left eigenspace is computed, i.e. vectors
+Return a matrix whose rows (if `side == :left`) or columns (if `side == :right`)
+give a basis of the eigenspace of $M$ with respect to the eigenvalue $\lambda$.
+If `side` is `:right`, the right eigenspace is computed, i.e. vectors $v$ such that
+$Mv = \lambda v$. If `side` is `:left`, the left eigenspace is computed, i.e. vectors
 $v$ such that $vM = \lambda v$.
 """
 function eigenspace(M::MatElem{T}, lambda::T; side::Symbol = :left) where T <: FieldElem
